cd57ff33e24e4fc2facd7a9eac0a4aab614d236c
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-18 12:59:00 -0700

    commit tree c66e0f9ffcd706769bfd977029877436b7b37971

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  count = 0
+  while count < collection.length
+    element = collection[count]
+  yield(element)
+  count += 1
+  end
+  return collection
+end
+
+my_each(caller_locations) do |i|
+  return i
 end
